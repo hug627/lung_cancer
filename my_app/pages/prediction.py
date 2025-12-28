bc70c87a142fecf2c22e9cd086667b02b27bb7f8
hug627 <katunge22.mercy@student.cuk.ac.ke>
2025-12-28 11:33:27 +0300

    Update prediction.py

--- my_app/pages/prediction.py
+++ my_app/pages/prediction.py
@@ -1,8 +1,9 @@
 import streamlit as st   
 import pandas as pd   
 import numpy as np 
-import joblib 
+import joblib
 import os
+import traceback
 
 # ===============================
 # PAGE CONFIG
